b7cea9dc08736d768090437270d951c8b8532008
beibeixiaomo <wenyong@sf-express.com>
2017-12-15 15:49:18 +0800

    修改方法复杂度

--- tarsier-app/src/main/java/com/sf/tarsier/mvc/service/MarketUsersService.java
+++ tarsier-app/src/main/java/com/sf/tarsier/mvc/service/MarketUsersService.java
@@ -27,7 +27,6 @@ public class MarketUsersService extends BaseService {
 	 */
 	public Result<Object> saveMarketUsers(MarketUsers users) {
 		try {
-			String returnMsg = "参团成功！";
 			//检查当前要参团的时间周期和参团人员
 			@SuppressWarnings("unchecked")
 			Map<String,Long> check = (Map<String,Long>)getBaseDAO().selectOne("MarketUsersMapper.selectMarketUsersLimit", users.getMktId());
@@ -36,28 +35,10 @@ public class MarketUsersService extends BaseService {
 				//参团数据不存在
 				return ResultUtil.error("集货拼团信息不存在，请进入拼团首页后重试！","marketNull");
 			}
-			else if(check.containsKey("is_passed") && check.get("is_passed") < 0 )
-			{
-				//旧团已过期，自动创建新团，并加入新团
-				String uuid = marketBaseService.createNewMarket();
-				users.setMktId(uuid);
-				returnMsg = "集货旧团已过期，自动拼入新团且操作成功！";
-			}
-			else if(check.containsKey(LEAVE_COUNT))
-			{
-				if(check.get(LEAVE_COUNT) <= 0)
-				{
-					//旧团人数已满，自动创建新团，并加入新团
-					String uuid = marketBaseService.createNewMarket();
-					users.setMktId(uuid);
-					returnMsg = "集货旧团已满员，自动拼入新团且操作成功！";
-				}
-				else if(check.get(LEAVE_COUNT) == 1)
-				{
-					//旧团人数将满，自动创建新团
-					marketBaseService.createNewMarket();
-				}
-			}
+			
+			//检查用户参团的，拼团信息
+			String returnMsg = checkJoinMarketInfo(check, users);
+			
 			//参团人员保存
 			getBaseDAO().update("MarketUsersMapper.saveMarketUsers", users);
 			return ResultUtil.success(returnMsg);
@@ -65,6 +46,40 @@ public class MarketUsersService extends BaseService {
 			logger.error("集货拼团，参团操作失败", e);
 			return ResultUtil.error("集货拼团参团操作失败，请重试！");
 		}
+	}
+	
+	/**
+	 * 检查用户参团的，拼团信息
+	 * @param check
+	 * @param users
+	 * @return
+	 */
+	private String checkJoinMarketInfo(Map<String,Long> check,MarketUsers users)
+	{
+		String returnMsg = "参团成功！";
+		if(check.containsKey("is_passed") && check.get("is_passed") < 0 )
+		{
+			//旧团已过期，自动创建新团，并加入新团
+			String uuid = marketBaseService.createNewMarket();
+			users.setMktId(uuid);
+			returnMsg = "集货旧团已过期，自动拼入新团且操作成功！";
+		}
+		else if(check.containsKey(LEAVE_COUNT))
+		{
+			if(check.get(LEAVE_COUNT) <= 0)
+			{
+				//旧团人数已满，自动创建新团，并加入新团
+				String uuid = marketBaseService.createNewMarket();
+				users.setMktId(uuid);
+				returnMsg = "集货旧团已满员，自动拼入新团且操作成功！";
+			}
+			else if(check.get(LEAVE_COUNT) == 1)
+			{
+				//旧团人数将满，自动创建新团
+				marketBaseService.createNewMarket();
+			}
+		}
+		return returnMsg;
 	}
 	
 }
