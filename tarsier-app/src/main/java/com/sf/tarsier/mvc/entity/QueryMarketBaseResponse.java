4f4ec1c9b122a22edca4e42f2e74406203a0354d
lihairong <lihairong.cn@gmail.com>
2017-12-15 13:49:59 +0800

    拼团信息查询

--- tarsier-app/src/main/java/com/sf/tarsier/mvc/entity/QueryMarketBaseResponse.java
+++ tarsier-app/src/main/java/com/sf/tarsier/mvc/entity/QueryMarketBaseResponse.java
@@ -1,24 +1,45 @@
 package com.sf.tarsier.mvc.entity;
 
 import java.util.List;
-
+import java.util.Map;
 
 public class QueryMarketBaseResponse {
-	/**
-	 * 拼团信息
-	 */
-	private MarketBase marketBase;
-	
+
 	/**
 	 * 参团人数
 	 */
 	private Integer userCount;
 
 	/**
-	 * 参团列表
+	 * 剩余人数
+	 * 
 	 * @return
 	 */
-	private List<MarketUsers> users;
+	private Integer freeCount;
+
+	/**
+	 * 占比
+	 * 
+	 * @return
+	 */
+	private Integer completePercent;
+
+	/**
+	 * 截止日期
+	 */
+	private String deadline;
+
+	/**
+	 * 拼团信息
+	 */
+	private MarketBase marketBase;
+
+	/**
+	 * 参团列表
+	 * 
+	 * @return
+	 */
+	private List<Map<String, String>> users;
 
 	public MarketBase getMarketBase() {
 		return marketBase;
@@ -36,11 +57,35 @@ public class QueryMarketBaseResponse {
 		this.userCount = userCount;
 	}
 
-	public List<MarketUsers> getUsers() {
+	public List<Map<String, String>> getUsers() {
 		return users;
 	}
 
-	public void setUsers(List<MarketUsers> users) {
+	public void setUsers(List<Map<String, String>> users) {
 		this.users = users;
+	}
+
+	public Integer getFreeCount() {
+		return freeCount;
+	}
+
+	public void setFreeCount(Integer freeCount) {
+		this.freeCount = freeCount;
+	}
+
+	public Integer getCompletePercent() {
+		return completePercent;
+	}
+
+	public void setCompletePercent(Integer completePercent) {
+		this.completePercent = completePercent;
+	}
+
+	public String getDeadline() {
+		return deadline;
+	}
+
+	public void setDeadline(String deadline) {
+		this.deadline = deadline;
 	}
 }
--- tarsier-app/src/main/java/com/sf/tarsier/mvc/service/MarketBaseService.java
+++ tarsier-app/src/main/java/com/sf/tarsier/mvc/service/MarketBaseService.java
@@ -1,15 +1,19 @@
 package com.sf.tarsier.mvc.service;
 
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
+import org.apache.commons.lang.time.DateFormatUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
 
 import com.alibaba.fastjson.JSON;
 import com.sf.tarsier.mvc.entity.MarketBase;
-import com.sf.tarsier.mvc.entity.MarketUsers;
 import com.sf.tarsier.mvc.entity.QueryMarketBaseRequest;
 import com.sf.tarsier.mvc.entity.QueryMarketBaseResponse;
 import com.sf.tarsier.mvc.system.base.BaseService;
@@ -24,20 +28,34 @@ public class MarketBaseService extends BaseService {
 	
 	public Result<Object> queryMarketBaseInfo(QueryMarketBaseRequest request){
 		QueryMarketBaseResponse response = new QueryMarketBaseResponse();
-		MarketBase marketBase = (MarketBase) getBaseDAO().selectOne("MarketBaseMapper.selectMarketBaseInfo", request.getMktId());
+		MarketBase marketBase = (MarketBase) getBaseDAO().selectOne("MarketBaseMapper.selectMarketBaseInfo", request);
 		String marketBaseJson = JSON.toJSONString(marketBase);
 		logger.info("marketBase :{}", marketBaseJson);
-		
-		if(Objects.nonNull(request.getMktId())){
-			// 拼团超时 或者 拼团满人数
-			return ResultUtil.error("100001", "newMarket");
-		}
 
 		// 根据mktId查询参团的人数和头像地址
-		List<MarketUsers> userList = getBaseDAO().selectList("MarketBaseMapper.selectMarketUsersByMktId", marketBase.getMktId());
+		List<Map<String, String>> users = getBaseDAO().selectList("MarketBaseMapper.selectMarketUsersByMktId", marketBase.getMktId());
 		response.setMarketBase(marketBase);
-		response.setUserCount(userList.size());
-		response.setUsers(userList);
+		response.setUserCount(users.size());
+		response.setUsers(users);
+
+		response.setFreeCount(marketBase.getGroupLimit() - users.size());
+		response.setCompletePercent(100 * users.size()/marketBase.getGroupLimit());
+		// 计算截止日期
+		Calendar calendar=Calendar.getInstance();   
+		calendar.setTime(marketBase.getCreateTime()); 
+		calendar.set(Calendar.MINUTE,calendar.get(Calendar.MINUTE) + marketBase.getGroupDuration());  
+		response.setDeadline(DateFormatUtils.format(calendar.getTime(), "yyyy-MM-dd HH:mm:ss"));
+		
+		if(Objects.nonNull(request) && !StringUtils.isEmpty(request.getMktId())){
+			// 拼团满人数
+			if(marketBase.getGroupLimit() <= users.size()){
+				return ResultUtil.error("100001", "newMarket");
+			}
+			// 拼团超时
+			if(calendar.getTime().compareTo(new Date()) < 0){
+				return ResultUtil.error("100002", "newMarket");
+			}
+		}
 		return ResultUtil.success(response);
 	}
 		
